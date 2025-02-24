be29308c656a3ed9765a896969e31369bf09d0b5
Pratik Karna <kaystha.pratik@gmail.com>
2025-02-24 22:37:13 +0530

    Added React Helmet POC

---
+++ react-helmet-demo/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HelmetExample from "./HelmetExample";
+
+function App() {
+    return ( <
+        div >
+        <
+        HelmetExample / >
+        <
+        <
+        /div>
+    );
+}
+
+export default App;
